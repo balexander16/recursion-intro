9bf79300af4ea2247410a15d666f7b73ad2e1f94
Brian <babta29@gmail.com>
2019-06-03 14:09:23 -0600

    Added Fibonacci Class

--- test/edu/cnm/deepdive/PalindromesTest.java
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -8,11 +8,25 @@ public class PalindromesTest {
 
   @Test
   public void isPalindrome() {
-    String s = "A man, a plan, a canal - Panama!";
-    assertTrue(Palindromes.isPalindrome(s));
+
+    assertTrue(Palindromes.isPalindrome("A man, a plan, a canal - Panama!"));
     assertTrue(Palindromes.isPalindrome("Dammit I'm Mad!"));
     assertTrue(Palindromes.isPalindrome("Madam I'm Adam"));
     assertFalse(Palindromes.isPalindrome("Blah blah blah"));
+    assertTrue(Palindromes.isPalindrome("radar"));
+    assertFalse(Palindromes.isPalindrome("away"));
+    assertTrue(Palindromes.isPalindrome("racecar"));
+    assertTrue(Palindromes.isPalindrome("eeeee"));
+    assertTrue(Palindromes.isPalindrome("a"));
+    assertFalse(Palindromes.isPalindrome("hi"));
+    assertTrue(Palindromes.isPalindrome("eve"));
+    assertFalse(Palindromes.isPalindrome("eve is eve"));
+    assertTrue(Palindromes.isPalindrome(""));
+    assertFalse(Palindromes.isPalindrome("1234"));
+    assertTrue(Palindromes.isPalindrome("deified"));
+    assertTrue(Palindromes.isPalindrome("92139455493129"));
+    assertFalse(Palindromes.isPalindrome("9070120709"));
+
   }
 
 
